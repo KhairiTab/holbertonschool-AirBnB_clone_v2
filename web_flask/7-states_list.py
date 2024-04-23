743ab40af450522c22b12754cc09b7d57cbd477d
KhairiTab <taboubikhairi68@gmail.com>
2024-04-23 18:16:22 +0100

    List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """returns a list of all states in the database"""
+    from models import storage
+    from models.state import State
+
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage on teardown"""
+    from models import storage
+
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
